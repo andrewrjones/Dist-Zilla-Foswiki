7db83db3e03a4d4f8c40dac1b2f78dd88e16556d
Andrew Jones <andrew.jones@arm.com>
2011-12-14 13:17:40 +0000

    intial plugin bundle for Foswiki

---
+++ lib/Dist/Zilla/PluginBundle/FOSWIKI.pm
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::PluginBundle::FOSWIKI;
+
+# ABSTRACT: L<Dist::Zilla> plugins for Foswiki development
+
+use Moose;
+use Moose::Autobox;
+use Dist::Zilla 2.100922;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+=for stopwords Prereqs CPAN
+=head1 DESCRIPTION
+
+This is the plugin bundle for Foswiki development. It is equivalent to:
+
+  [GatherDir]
+
+=cut
+
+# Alphabetical
+use Dist::Zilla::Plugin::GatherDir;
+
+=for Pod::Coverage configure
+=cut
+
+sub configure {
+    my ($self) = @_;
+
+    $self->add_plugins(
+        qw(
+          GatherDir
+          )
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
